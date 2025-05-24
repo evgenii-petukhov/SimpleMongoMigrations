0a429fa6e22e05c6ce2e60e4beccc5cf7ca624f6
Evgenii Petukhov <evgenii.petukhov@gameteq.com>
2025-05-24 19:32:35 +0200

    Added unit tests for MigrationScanner

---
+++ SimpleMongoMigrations.Tests/MigrationScannerTests.cs
@@ -0,0 +1,106 @@
+﻿using FluentAssertions;
+using MongoDB.Driver;
+using SimpleMongoMigrations.Abstractions;
+using SimpleMongoMigrations.Attributes;
+using System.Reflection;
+using IgnoreAttribute = SimpleMongoMigrations.Attributes.IgnoreAttribute;
+
+namespace SimpleMongoMigrations.Tests
+{
+    [TestFixture]
+    public class MigrationScannerTests
+    {
+        private Assembly _testAssembly;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _testAssembly = typeof(MigrationScannerTests).Assembly;
+        }
+
+        [Test]
+        public void Migrations_ShouldReturnTypesWithVersionAttribute_AndImplementIMigration()
+        {
+            var scanner = new MigrationScanner(_testAssembly);
+            var migrations = scanner.Migrations;
+
+            migrations.Should().Contain(typeof(TestMigration1));
+            migrations.Should().Contain(typeof(TestMigration2));
+        }
+
+        [Test]
+        public void Migrations_ShouldNotReturnAbstractClasses()
+        {
+            var scanner = new MigrationScanner(_testAssembly);
+            var migrations = scanner.Migrations;
+
+            migrations.Should().NotContain(typeof(AbstractMigration));
+        }
+
+        [Test]
+        public void Migrations_ShouldNotReturnClassesWithoutVersionAttribute()
+        {
+            var scanner = new MigrationScanner(_testAssembly);
+            var migrations = scanner.Migrations;
+
+            migrations.Should().NotContain(typeof(NoVersionMigration));
+        }
+
+        [Test]
+        public void Migrations_ShouldNotReturnClassesWithIgnoreAttribute()
+        {
+            var scanner = new MigrationScanner(_testAssembly);
+            var migrations = scanner.Migrations;
+
+            migrations.Should().NotContain(typeof(IgnoredMigration));
+        }
+
+        [Test]
+        public void Migrations_ShouldReturnClassesOrderedByVersion()
+        {
+            var scanner = new MigrationScanner(_testAssembly);
+            var migrations = scanner.Migrations;
+
+            migrations.IndexOf(typeof(TestMigration1)).Should().BeLessThan(migrations.IndexOf(typeof(TestMigration2)));
+        }
+
+        [Test]
+        public void Migrations_ShouldReturnEmptyList_WhenNoValidMigrations()
+        {
+            var scanner = new MigrationScanner(typeof(object).Assembly); // mscorlib, no migrations
+            var migrations = scanner.Migrations;
+
+            migrations.Should().BeEmpty();
+        }
+
+        [Version("1.0.0")]
+        private class TestMigration1 : IMigration
+        {
+            public void Up(IMongoDatabase database) { }
+        }
+
+        [Version("2.0.0")]
+        private class TestMigration2 : IMigration
+        {
+            public void Up(IMongoDatabase database) { }
+        }
+
+        [Version("3.0.0")]
+        private abstract class AbstractMigration : IMigration
+        {
+            public abstract void Up(IMongoDatabase database);
+        }
+
+        private class NoVersionMigration : IMigration
+        {
+            public void Up(IMongoDatabase database) { }
+        }
+
+        [Ignore]
+        [Version("4.0.0")]
+        private class IgnoredMigration : IMigration
+        {
+            public void Up(IMongoDatabase database) { }
+        }
+    }
+}
